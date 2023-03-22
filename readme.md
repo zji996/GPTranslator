0457f840688816e822e0f9fabad7a1755b7be6e3
zji996 <jzy896370029@gmail.com>
2023-03-22 21:51:27 +0800

    fixed

--- readme.md
+++ readme.md
@@ -10,7 +10,8 @@ $ pip install -r requirments
 
 ## Usage
 Change the `run.py` file to your needs.
-
+put your srt subtitle files to srt folder.
 ```sh
 $ python run.py
 ```
+you can see translated subtitle in the `srtt` folder.
--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import glob
 from subtitle_translator import SubsTranslator
 playrole = 'to act as an translator, spelling corrector and improver. The content I provide to you, you will translate it and answer in the corrected and improved version of my text, Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations.'
 #以后可能加更多人设进行翻译，现在就先这样吧
-language = '中文'   # default language
-output_dir = './srtt'
+language = '中文'   # default language 
+output_dir = './srtt' # output directory
 OpenAI_API = ' ' # OpenAI API 这里输入你的API
 srt_files = glob.glob('./srt/**/*.srt', recursive=True)
 progress_file = './progress.json'
