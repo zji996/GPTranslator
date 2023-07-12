59176c0267645e77fd7ba4848f0d8c68f21e24a0
括号 <jzy896370029@gmail.com>
2023-07-12 13:53:04 +0800

    update

--- subtitle_translator.py
+++ subtitle_translator.py
@@ -4,7 +4,7 @@ import time
 import json
 import os
 def process_text(text, target_language, api_key, 
-                   playrole,task, max_retries=3, retry_interval=3):
+                   playrole,task,max_tokens=250, max_retries=3, retry_interval=3):
     openai.api_key = api_key
     retries = 0
     
@@ -12,6 +12,8 @@ def process_text(text, target_language, api_key,
         try:
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
+                temperature=0.1,
+                max_tokens=max_tokens,
                 messages=[
                     {
                         "role": "system",
@@ -71,10 +73,12 @@ def SubsTranslator(src, des, targetlanguage, API, playrole='',task='Translate th
     for idx, sub in enumerate(subs[start_index:]):
         original_text = sub.text
         glossary_terms = find_glossary_terms(original_text, glossary)
-        glossary_text = " ".join([f"{term}: {definition}" for term, definition in glossary_terms])
-        playrole_with_glossary = f"{playrole} ,The following is a glossary: {glossary_text}" if glossary_terms else playrole
         if glossary_terms:
+            glossary_text = " ".join([f"{term}: {definition}" for term, definition in glossary_terms])
+            playrole_with_glossary = f"{playrole} ,The following is a glossary: {glossary_text}"
             print(f"Found glossary terms: {glossary_terms}")
+        else:
+            playrole_with_glossary = playrole
         translated_text = process_text(original_text, targetlanguage, API, playrole_with_glossary,task).replace('。', ' ').replace('，', ' ').replace('\"', '').replace('“', '').replace('”', '')
         if translated_text:
             sub.text = f"{translated_text}\\n{original_text}"
@@ -84,9 +88,7 @@ def SubsTranslator(src, des, targetlanguage, API, playrole='',task='Translate th
             save_progress(progress_file, progress)
         else:
             print(f"Failed to translate subtitle index {start_index + idx}: {original_text}")
-        time.sleep(0.5)
-
-    print(f"Translated {src} to {des}.")
+        time.sleep(0.1)
 
 def SubsSummarizer(src, des, target_language, api_key, playrole='', task='Summarize the following text to', start_index=0, end_index=None):
     subs = pysrt.open(src)
